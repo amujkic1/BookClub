ddee49c926d935dba3bfbb5753bde38a63a83bf2
amujkic1 <amujkic1@etf.unsa.ba>
2024-09-03 19:37:54 +0200

    added routes for book creation

--- server/models/bookModel.js
+++ server/models/bookModel.js
@@ -10,34 +10,28 @@ const bookSchema = new Schema({
         type: String,
         required: true
     },
-/*    publishDate: {
+    publishDate: {
         type: Date,
-        required: true
     },
     isbn: {
         type: String,
         unique: true,
-        required: true
     },
     genre: {
         type: String,
-        required: true
     },
     summary: {
         type: String,
-        required: true
     },
     language: {
         type: String,
-        required: true
     },
     pages: {
         type: Number,
-        required: true
     },
     coverImageUrl: {
         type: String,
-    }*/
+    }
 })
 
 module.exports = mongoose.model('Book', bookSchema);
--- server/routes/books.js
+++ server/routes/books.js
@@ -1,17 +1,8 @@
 const express = require('express')
 const Book = require('../models/bookModel')
 const router = express.Router()
+const bookController = require('../controllers/bookController')
 
-router.post('/book', async (req, res) => {
-    const{title, author} = req.body
-    
-    try{
-        const book = await Book.create({title,author})
-        res.status(200).json(book)
-    }catch(err){
-        console.log(err)
-        res.status(400).json({error: err.message})
-    }
-})
+router.post('/book', bookController.createBook)
 
 module.exports = router
