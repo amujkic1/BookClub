d38299d9847b0e0f3d49e6a5d99734b925b7b9ae
amujkic1 <amujkic1@etf.unsa.ba>
2024-09-16 16:31:34 +0200

    added backend route for book retrieving by id

--- server/controllers/chatContoller.js
+++ server/controllers/chatContoller.js
@@ -1,6 +1,6 @@
 module.exports = (io) => {
     io.on("connection", (socket) => {
-      console.log("A user connected", socket.id);
+      //console.log("A user connected", socket.id);
   
       socket.on("chatMessage", ({username, message, time}) => {
         io.emit("chatMessage", {username, message, time}); 
