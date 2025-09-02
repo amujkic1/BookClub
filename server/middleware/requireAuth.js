167f05133500de2d685fed2fde31828d7aa1d5d5
amujkic1 <amujkic1@etf.unsa.ba>
2025-09-02 14:12:30 +0200

    role based acces

--- server/middleware/requireAuth.js
+++ server/middleware/requireAuth.js
@@ -1,17 +1,33 @@
 const jwt = require("jsonwebtoken");
+const User = require("../models/userModel")
 
-function requireAuth(req, res, next) {
-  const token = req.cookies.jwt;
+async function requireAuth(req, res, next) {
 
+const token = req.cookies.jwt;
   if (!token) return res.status(401).json({ error: "Not authenticated" });
 
   try {
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
-    req.user = decoded;
+    const user = await User.findById(decoded._id);
+
+    if (!user) {
+      return res.status(401).json({ error: "User not found" });
+    }
+
+    req.user = user; 
     next();
   } catch (err) {
     return res.status(403).json({ error: "Invalid token" });
   }
 }
 
-module.exports = { requireAuth };
+function requireRole(role) {
+  return (req, res, next) => {
+    if (req.user.role !== role) {
+      return res.status(403).json({ error: "Forbidden" });
+    }
+    next();
+  };
+}
+
+module.exports = { requireAuth, requireRole };
--- server/routes/books.js
+++ server/routes/books.js
@@ -1,10 +1,10 @@
 const express = require('express')
 const router = express.Router()
 const bookController = require('../controllers/bookController')
-const { requireAuth } = require('../middleware/requireAuth')
+const { requireAuth, requireRole } = require('../middleware/requireAuth')
 
 router.post('/book', bookController.createBook)
-router.get('/books', bookController.getAllBooks)
+router.get('/books', requireAuth, requireRole("User"), bookController.getAllBooks)
 router.get('/books/:bookId', bookController.getBookById)
 router.delete('/books/:bookId', bookController.deleteBook)
 router.post('/add', requireAuth, bookController.addBookToList)
